17011c540431904eaa6b8ed4909e8b59343bc3a9
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2022-04-24 19:34:31 +0200

    added test event listener

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const swup = new Swup();
+const swup = new Swup({cache: false});
+
+swup.on('pageView', () => console.log('pageView'));
+swup.on('contentReplaced', () => console.log('contentReplaced'));
+swup.on('pageLoaded', () => console.log('pageLoaded'));
+swup.on('pagePreloaded', () => console.log('pagePreloaded'));
